064fd114958c4c887f28b3dfe9b2e7b2f0803259
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-08-15 19:27:24 -0400

    Removed logging calls.

--- display/js/TankModelLoader.js
+++ display/js/TankModelLoader.js
@@ -5,18 +5,12 @@ var TankModelLoader = function() {
 
   // Private static functions
   function loadMesh() {
-    console.log("setting up the load");
-
     return JSONLoader.load('/json/tank.json').then(function(body_mesh) {
 
-      console.log("Loaded the body.");     
- 
       body = body_mesh;
       return JSONLoader.load('/json/barrel.json');
     
     }).then(function(barrel) {
-
-      console.log("Loaded the barrel");
 
       body.add(barrel);
       return body;
@@ -25,7 +19,6 @@ var TankModelLoader = function() {
 
   // Public static functions
   that.load = function() {
-    console.log("Loading tank model");
     return loadMesh();
   }
 
