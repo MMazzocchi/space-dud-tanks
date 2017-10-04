cdee354f4a0b66c49a22a204bd31a6823fd82aa5
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-10-03 20:34:03 -0400

    Added methods to Game/TankGame to begin making them the clock controller.

--- server/game/Game.js
+++ server/game/Game.js
@@ -1,32 +1,56 @@
+var EventEmitter = require('events');
+
 var Game = function() {
   var that = {};
+  var clock = new EventEmitter();
 
   // Fields
-  var rooms = {};
+  var room_hash = {};
+  var last_update = new Date();
+
+  // Private methods
+  function tick() {
+    var now = new Date();
+    var delta = now - last_update;
+
+    clock.emit('tick', delta, now);
+    last_update = now;
+
+    setImmediate(tick);
+  };
 
   // Public methods
+  that.addRoom = function(room) {
+    clock.on('tick', room.tick);
+  };
+
+  that.removeRoom = function(room) {
+    clock.removeListener('tick', room.tick);
+  };
+
   that.registerRoom = function(name, room) {
-    rooms[name] = room;
+    room_hash[name] = room;
   };
 
   that.getRoom = function(name) {
-    return rooms[name];
+    return room_hash[name];
   };
 
   that.start = function() {
-    var names = Object.keys(rooms);
+    var names = Object.keys(room_hash);
     names.forEach(function(name) {
-      rooms[name].start();
+      room_hash[name].start();
     });
   };
 
   that.stop = function() {
-    var names = Object.keys(rooms);
+    var names = Object.keys(room_hash);
     names.forEach(function(name) {
-      rooms[name].stop();
+      room_hash[name].stop();
     });
   };
 
+  setImmediate(tick);
   return that;
 };
 
--- server/game/TankGame.js
+++ server/game/TankGame.js
@@ -3,10 +3,16 @@ var ArenaRoom = require('./rooms/ArenaRoom.js');
 
 var TankGame = function() {
   var that = new Game();
+  var arena = new ArenaRoom();
 
   // Private functions
   function setup() {
     that.registerRoom('arena', new ArenaRoom());
+  };
+
+  // Public functions
+  that.addPlayer = function(player, color) {
+    return arena.addPlayer(player, color);
   };
 
   setup();
