0bbccdf11fc522279ce3981309c77bbc622a9db0
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-10-04 20:53:54 -0400

    Removed all old code from main.js.

--- display/src/main.js
+++ display/src/main.js
@@ -1,33 +1,6 @@
 var Game = require('./Game.js');
-m
 
 $(function() {
-  function toggleFullScreen() {
-
-    var doc = window.document;
-    var docEl = doc.documentElement;
-  
-    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
- 
-    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-      requestFullScreen.call(docEl);
-    }
-    else {
-      cancelFullScreen.call(doc);
-    }
-  };
-
-  function setupGame(game) {
-    window.addEventListener( 'resize', function() {
-      game.setSize(window.innerWidth, window.innerHeight);
-    }, false );
-
-//    var domElement = game.getDomElement();
-//    document.body.appendChild(domElement);
-//    domElement.ondblclick = toggleFullScreen;
-  }
-
   function start() {
     var client = new DisplayConnection();
     client.on('player_chosen', function(valid) {
@@ -45,16 +18,6 @@ $(function() {
       var player_id = document.getElementById('player_id').value;
       client.selectPlayer(player_id);
     };
-
-//        document.body.innerHTML = "";
-//        var game = new Game(document.body, window.innerWidth, window.innerHeight, client);
-//
-//        client.on('player_chosen', function(valid) {
-//          if(valid) {
-//            setupGame(game);
-//          }
-//        });
-//        client.selectPlayer(player_id);
   }
 
   start();
