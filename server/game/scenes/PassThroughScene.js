663db61176175e729cbbb6c06fb238b2ceab36a2
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-10-05 21:41:36 -0400

    Refactored Scene structure to send a broadcast when a new scene starts, and send a packet when a new consumer joins.

--- server/game/scenes/PassThroughScene.js
+++ server/game/scenes/PassThroughScene.js
@@ -1,7 +1,14 @@
-var PassThroughScene = async function(player) {
-   return new Promise(function(resolve, reject) {
-     player.on('controller_event', player.sendEventToConsumers);
-   });
+var Scene = require('./Scene.js');
+
+var PassThroughScene = function(player) {
+
+  function setupPassThrough() {
+    return new Promise(function(resolve, reject) {
+      player.on('controller_event', player.sendEventToConsumers);
+    });
+  };
+
+  Scene('pass_through', player).then(setupPassThrough);
 };
 
 module.exports = PassThroughScene;
--- server/game/scenes/Scene.js
+++ server/game/scenes/Scene.js
@@ -1,7 +1,28 @@
-var Scene = function() {
-  var that = new Promise(function(resolve, reject) {
+var Scene = async function(name, player) {
 
-  });
+  function createScenePacket() { 
+    var data = {
+      'event_type': 'scene',
+      'scene': name
+    };
+
+    return data;
+  };
+
+  function broadcastScenePacket() {
+    var data = createScenePacket();
+    player.sendEventToConsumers(data);
+  };
+
+  function sendScenePacket(consumer_id) {
+    setImmediate(function() {
+      var data = createScenePacket();
+      player.sendEventToConsumer(data, consumer_id);
+    });
+  };
+
+  setImmediate(broadcastScenePacket);
+  player.on('consumer_added', sendScenePacket);
 };
 
 module.exports = Scene;
