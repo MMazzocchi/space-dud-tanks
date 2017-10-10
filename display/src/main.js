0d6959d5276b72bbe8f6387d33c4e14b8a228418
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-10-09 20:41:04 -0400

    Removed some unneeded main code.

--- display/src/main.js
+++ display/src/main.js
@@ -1,20 +1,16 @@
 var GameClient = require('./GameClient.js');
 
 $(function() {
-  function start() {
-    var game_client = new GameClient(document.body);
+  var game_client = new GameClient();
 
-    document.getElementById('submit_player_id').onclick = function(e) {
-      var player_id = document.getElementById('player_id').value;
+  document.getElementById('submit_player_id').onclick = function(e) {
+    var player_id = document.getElementById('player_id').value;
 
-      game_client.useDisplayConnection(player_id).then(function() {
-        console.log("It worked!!!");
+    game_client.useDisplayConnection(player_id).then(function() {
+      console.log("It worked!!!");
 
-      }).catch(function() {
-        console.log("It didn't work :(");
-      });
-    };
-  }
-
-  start();
+    }).catch(function() {
+      console.log("It didn't work :(");
+    });
+  };
 });
