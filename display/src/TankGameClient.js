19c87a204e280d0c291b84a1cdab4e0e86e2513c
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-10-10 21:47:01 -0400

    Added LogScene to TankGameSetup.

--- display/src/TankGameClient.js
+++ display/src/TankGameClient.js
@@ -1,11 +1,12 @@
 var GameClient = require('./GameClient.js');
+var LogScene = require('./scene/LogScene.js');
 
 var TankGameClient = function() {
   var that = new GameClient();
 
   that.connectionReady = function(connection) {
     var scene_switcher = new SceneSwitcher(connection);
-
+    scene_switcher.addScene('controller_setup', new LogScene(connection));
   };
 
   return that;
