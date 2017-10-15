855739563c99ce9c833883e9162aff4ad3532953
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-10-15 10:20:36 -0400

    Setup platform and lighting in ColorSelectScene.

--- display/src/scene/ColorSelectScene.js
+++ display/src/scene/ColorSelectScene.js
@@ -1,9 +1,9 @@
-var LogScene = require('./LogScene.js');
+var Scene = require('./Scene.js');
 var AnimatedScene = require('./AnimatedScene.js');
 var THREE = require('../../lib/three.min.js');
 
 var ColorSelectScene = function(canvas_switcher, connection) {
-  var that = AnimatedScene.mixin(new LogScene(connection));
+  var that = AnimatedScene.mixin(new Scene());
   var canvas = canvas_switcher.get3dCanvas();
 
   // Fields
@@ -15,14 +15,43 @@ var ColorSelectScene = function(canvas_switcher, connection) {
   var renderer = new THREE.WebGLRenderer({ canvas: canvas });
   
   // Private methods
-  function resize() {
+  function setupLighting() {
+    var am_light = new THREE.AmbientLight( 0x707070 );
+    scene.add(am_light);
+
+    var spot_light = new THREE.SpotLight( 0xDDDDDD );
+    spot_light.angle = Math.PI / 9;
+    spot_light.penumbra = 0.5;
+    spot_light.position.y += 30;
+    scene.add(spot_light);
+  };
+
+  function setupPlatform() {
+    var geom = new THREE.PlaneGeometry(20.0, 20.0, 5, 5);
+    var mat = new THREE.MeshPhongMaterial( { color: 0xFFFFFF } );
+    mat.side = THREE.DoubleSide;
+
+    var platform = new THREE.Mesh(geom, mat);
+    platform.rotation.x -= Math.PI / 2;
+    platform.rotation.z += Math.PI/4;
+    scene.add(platform);
+  };
+
+  function setup() {
     width = canvas.width;
     height = canvas.height;
+
+    renderer.setSize(width, height);
 
     camera.aspect = width/height;
     camera.updateProjectionMatrix();
 
-    renderer.setSize(width, height);
+    camera.position.z += 12; 
+    camera.position.y += 11;
+    camera.rotation.x -= Math.PI / 4;
+
+    setupPlatform();
+    setupLighting();
   };
 
   that.on('setup', function() {
@@ -31,15 +60,10 @@ var ColorSelectScene = function(canvas_switcher, connection) {
 
   // Public methods
   that.draw = function() {
-    if((canvas.width  !== width ) ||
-       (canvas.height !== height)) {
-      resize();
-    }
-
     renderer.render(scene, camera);
   };
 
-  resize();
+  setup();
 
   return that;
 };
