af9bb4df1ee568f1c882c9d4688786d097f3a565
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-10-14 21:10:19 -0400

    Integration THREE.js with ColorSelectScene.

--- display/src/scene/ColorSelectScene.js
+++ display/src/scene/ColorSelectScene.js
@@ -1,41 +1,40 @@
 var LogScene = require('./LogScene.js');
 var AnimatedScene = require('./AnimatedScene.js');
+var THREE = require('../../lib/three.min.js');
 
 var ColorSelectScene = function(canvas, connection) {
   var that = AnimatedScene.mixin(new LogScene(connection));
 
   // Fields
-  var ctx = canvas.getContext('2d');
-  var count = 0;
+  var width = canvas.width;
+  var height = canvas.height;
 
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(75, 0.5, 0.1, 10000);
+  var renderer = new THREE.WebGLRenderer({ canvas: canvas });
+  
   // Private methods
-  function decToHex(dec) {
-    if(dec <   10) { return dec; }
-    if(dec === 10) { return "A"; }
-    if(dec === 11) { return "B"; }
-    if(dec === 12) { return "C"; }
-    if(dec === 13) { return "D"; }
-    if(dec === 14) { return "E"; }
-    if(dec === 15) { return "F"; }
+  function resize() {
+    width = canvas.width;
+    height = canvas.height;
+
+    camera.aspect = width/height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(width, height);
   };
 
   // Public methods
   that.draw = function() {
-    var width = canvas.width;
-    var height = canvas.height;
-
-    var color = "#";
-    var hex = decToHex(count);
-
-    for(var i=0; i<6; i++) {
-      color = color + hex;
+    if((canvas.width  !== width ) ||
+       (canvas.height !== height)) {
+      resize();
     }
 
-    ctx.fillStyle = color;
-    ctx.fillRect(0, 0, width, height);
-
-    count = (count + 1) % 16;
+    renderer.render(scene, camera);
   };
+
+  resize();
 
   return that;
 };
