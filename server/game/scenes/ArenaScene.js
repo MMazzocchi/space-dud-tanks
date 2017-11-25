c20c984d676cea679dfb77a574a45b829936d6f0
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-11-25 17:59:07 -0500

    Hooked up throttle, brake, fire.

--- server/game/scenes/ArenaScene.js
+++ server/game/scenes/ArenaScene.js
@@ -8,6 +8,9 @@ var ArenaScene = function(player, color, controller, room) {
 
       controller.on('left', tank.left);
       controller.on('right', tank.right);
+      controller.on('throttle', tank.throttle);
+      controller.on('brake', tank.brake);
+      controller.on('fire', tank.fire);
 
       player.on('disconnect', resolve);
     });
